c4172db26f60842f15fd4bb6eb057c12ffc8b73f
Ankit Kaushal <ankitkaushal288@gmail.com>
2024-05-20 01:20:28 +0530

    added effects

--- src/components/Navigation/index..tsx
+++ src/components/Navigation/index..tsx
@@ -1,17 +1,37 @@
+import React, { useState, useEffect } from "react";
 import { useTheme } from "../../common/ThemeContext";
-
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMoon } from "@fortawesome/free-solid-svg-icons";
+import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
 
 import styles from "./styles.module.css";
 
 const Navigation: React.FC = () => {
   const { theme, toggleTheme } = useTheme();
+  const [icon, setIcon] = useState(theme === "light" ? faMoon : faSun);
+  const [rotateIcon, setRotateIcon] = useState(false);
+
+  useEffect(() => {
+    if (rotateIcon) {
+      const timeout = setTimeout(() => {
+        setIcon(theme === "light" ? faMoon : faSun);
+        setRotateIcon(false);
+      }, 500);
+      return () => clearTimeout(timeout);
+    }
+  }, [rotateIcon, theme]);
+
+  useEffect(() => {
+    setRotateIcon(true);
+  }, [theme]);
+
   return (
     <div className={styles.navigation_container}>
       <div className={styles.navigation_header}>Where in the world?</div>
       <button onClick={toggleTheme} className={styles.toggle_button}>
-        <FontAwesomeIcon icon={faMoon} />
+        <FontAwesomeIcon
+          icon={icon}
+          className={rotateIcon ? styles.rotate : ""}
+        />
         {theme === "light" ? "Dark" : "Light"} Mode
       </button>
     </div>
